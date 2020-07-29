167eb7efd3a1f9f532b1caff8699d963fff11c4d
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-29 18:58:23 -0500

    Made rootReducer a function

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,10 @@
-const rootReducer = {
-
+const rootReducer = (state = '', action) => {
+    switch(action.type) {
+        case 'clear':
+            return 0
+        default: 
+            return state
+    }
 }
 
 export default rootReducer
