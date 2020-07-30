9132d042ca6a374ebfab0fc6a5f345b338ee6f0a
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-29 19:13:28 -0500

    Center content in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Row d-flex="justify-content-center">
+          <Col xs={{span:10, offset:1}} sm={{span: 4, offset: 4}} md={{ span: 6, offset:3 }} d-flex="justify-content-center">
+              Greetings!!
+          </Col>
+
+        </Row>
+        
+      </Container>
       
     </div>
   );
