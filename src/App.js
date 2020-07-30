04fc193bb94789444f33dfe888aee18b1a57f3dd
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-30 12:08:32 -0500

    Add elevator background image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import UserA from './Components/UserA'
 import UserB from './Components/UserB'
 import UserC from './Components/UserC'
 
+const bannerStyle = () => ({
+  backgroundImage: `url(${require(`./Images/city_skyscraper.jpg`)})`,
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  width: '100vw',
+  overflowY: 'hidden'
+})
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={bannerStyle()}>
       <Container>
         <Row d-flex="justify-content-center">
           <Col xs={{span:10, offset:1}} sm={{span: 4, offset: 4}} md={{ span: 6, offset:3 }} d-flex="justify-content-center">
