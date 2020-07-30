de328bfa3ba0f0b5a8ef15303078efacc33d46ed
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-30 15:14:07 -0500

    Set up handleChange state update for UserC component

--- src/Components/UserC.js
+++ src/Components/UserC.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState , Fragment } from 'react'
 import { connect } from 'react-redux'
 import { ListGroupItem, Button } from 'react-bootstrap'
 import UpdateUserCDestination from '../Actions/UpdateUserCDestination'
@@ -21,14 +21,19 @@ const mapStateToProps = state => {
   }
 }
 
-  function userC() {
+  function UserC() {
+
+    const [destinationFloor, setdestinationFloor ] = useState('')
+    const handleChange = event => setdestinationFloor(event.target.value)
         return(
             <Fragment>
                 <ListGroupItem>
                   <p className="small">User C: Current: <span className="text-primary">4</span> 
                   | Destination: <span className="text-primary">6</span> 
                   | <span className="text-primary">OFF
-                  </span> </p><input type="number" min="0" max="30"></input> 
+                  </span> </p><input type="number" min="0" max="30"
+                   value={destinationFloor} onChange={handleChange}>
+                     </input> 
                   <Button type="submit">Press Button</Button>
                   </ListGroupItem>
             </Fragment>
@@ -36,4 +41,4 @@ const mapStateToProps = state => {
     
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(userC)
+export default connect(mapStateToProps,mapDispatchToProps)(UserC)
