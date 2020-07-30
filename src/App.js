f8fca6ffc3604aad0da0efa5fc9f93dfed7a6fc5
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-29 20:01:31 -0500

    Set up card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Card, ListGroup, ListGroupItem, Input } from 'react-bootstrap'
 
 function App() {
   return (
@@ -7,7 +7,25 @@ function App() {
       <Container>
         <Row d-flex="justify-content-center">
           <Col xs={{span:10, offset:1}} sm={{span: 4, offset: 4}} md={{ span: 6, offset:3 }} d-flex="justify-content-center">
-              Greetings!!
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    Currrent Floor: 1
+                  </Card.Title>
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem>User A: Floor 4 </ListGroupItem>
+                    <ListGroupItem><input type="number"></input></ListGroupItem>
+                    <ListGroupItem>User B: Floor 4</ListGroupItem>
+                    <ListGroupItem><input type="number"></input></ListGroupItem>
+                    <ListGroupItem>User C: Floor 6</ListGroupItem>
+                    <ListGroupItem><input type="number"></input></ListGroupItem>
+                  </ListGroup>
+
+                </Card.Body>
+                <Card.Img variant="bottom" src="#">
+
+                </Card.Img>
+              </Card>
           </Col>
 
         </Row>
