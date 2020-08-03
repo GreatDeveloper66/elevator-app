1a00600a30adfb93d3719e74b2e59fc5ad43a162
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-03 16:25:33 -0500

    Add updating userA destination capability to reducer

--- src/reducers/userAReducer.js
+++ src/reducers/userAReducer.js
@@ -1,7 +1,7 @@
 const userAReducer = (state={Floor: 1,Destination: '',Status: 'OFF'},action) => {
     switch(action.type){
-      case 'setcategorylabel':
-        return action.categorylabel
+      case 'UpdateUserADestination':
+        return { ...state, Destination: action.newfloor }
       default:
         return state
     }
