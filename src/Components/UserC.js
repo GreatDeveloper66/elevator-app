f9dfa5a7efdc49054210aec1ea137af5127159b0
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-03 16:33:53 -0500

    user C destination floor now changed in redux store

--- src/Components/UserC.js
+++ src/Components/UserC.js
@@ -2,7 +2,6 @@ import React, { useState , Fragment } from 'react'
 import { connect } from 'react-redux'
 import { ListGroupItem, Button } from 'react-bootstrap'
 import UpdateUserCDestination from '../Actions/UpdateUserCDestination'
-import { propTypes } from 'react-bootstrap/esm/Image'
 
 
 const mapStateToProps = state => {
@@ -25,6 +24,9 @@ const mapStateToProps = state => {
 
     const [destinationFloor, setdestinationFloor ] = useState('')
     const handleChange = event => setdestinationFloor(event.target.value)
+    const handleSubmit = () => {
+      props.updateDestination(destinationFloor)
+    }
         return(
             <Fragment>
                 <ListGroupItem>
@@ -34,7 +36,7 @@ const mapStateToProps = state => {
                   </span> </p><input type="number" min="0" max="30"
                    value={destinationFloor} onChange={handleChange}>
                      </input> 
-                  <Button type="submit">Press Button</Button>
+                  <Button type="submit" onClick={handleSubmit}>Press Button</Button>
                   </ListGroupItem>
             </Fragment>
         )
--- src/reducers/userCReducer.js
+++ src/reducers/userCReducer.js
@@ -1,10 +1,10 @@
 const userCReducer = (state={Floor: 1,Destination: '',Status: 'OFF'},action) => {
-    switch(action.type){
-      case 'setcategorylabel':
-        return action.categorylabel
-      default:
-        return state
-    }
+  switch(action.type){
+    case 'UpdateUserCDestination':
+      return { ...state, Destination: action.newfloor }
+    default:
+      return state
   }
-  
-  export default userCReducer
+}
+
+export default userCReducer
