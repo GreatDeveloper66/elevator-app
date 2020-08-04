969b2ce5943bcee12a7b35dccb757a4cbc887b08
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-04 13:29:59 -0500

    Update redux logic

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,48 +1,84 @@
 const defaultState = {
     elevator: {
         floor: 6,
-        direction: 'N'
+        direction: 'N',
+        destination: null
     },
     userA: {
         floor: 1,
         destination: '',
-        status: 'OFF',
-        place: 'RD'
+        onelevator: false,
+        waiting: false
 
     },
     userB: {
         floor: 5,
         destination: '',
-        status: 'OFF',
-        place: 'RD'
+        onelevator: false,
+        waiting: false
     },
     userC: {
         floor: 8,
         destination: '',
-        status: 'OFF',
-        place: 'RD'
+        onelevator: false,
+        waiting: false
     }
     
 }
 
 const rootReducer = (state = defaultState, action) => {
-    let [ Efloor, Afloor, AStatus, ADestination, Bfloor, BStatus, BDestination, Cfloor, CStatus, CDestination, direction, Nfloor, newfloor ] = 
-        [ state.elevator.floor, state.userA.floor, state.userA.status, state.userA.destination,
+    let { elevatorFloor, elevatorDirection, elevatorDestination } = state.elevator
+    let { AFloor, ADestination, Aonelevator, Awaiting } = state.userA
+    let { BFloor, BDestination, Bonelevator, Bwaiting } = state.userB
+    let { CFloor, CDestination, Conelevator, Cwaiting } = state.userC
+    
+    /*
+    let [ Efloor, Afloor, AStatus, ADestination, 
+        APlace, Bfloor, BStatus, BDestination, 
+        Cfloor, CStatus, CDestination, direction, 
+        Nfloor, newfloor, EDestination ] = 
+        [ state.elevator.floor, state.userA.floor, state.userA.status, state.userA.destination,state.userA.place,
       state.userB.floor, state.userB.status, state.userB.destination, state.userC.floor,
-      state.userC.status, state.userC.destination, state.elevator.direction, state.elevator.floor, null ];
-
+      state.userC.status, state.userC.destination, state.elevator.direction, state.elevator.floor, null, state.elevator.destination ];
+            */
     switch(action.type) {
     case 'MoveElevatorFloor':
-        Nfloor = direction === "U" ? Efloor + 1 : direction === "D" ? Efloor - 1: Efloor
-        Afloor = AStatus === "ON" ? Nfloor : Afloor
-        Bfloor = BStatus === "ON" ? Nfloor: Bfloor
-        Cfloor = CStatus === "ON" ? Nfloor: Cfloor
+        //case1: userA is getting on elevator to got to destination
+        //case2: userA is getting off elevator after arriviing at destination
+        //case3: userA is going nowhere
+        const gap = elevatorDestination ? elevatorDestination - elevatorFloor : 0
+        const newDirection = gap > 0 ? "U" : gap < 0 ? "D" : "N"
+        Afloor = Aonelevator ? elevatorFloor : Afloor
+        Bfloor = Bonelevator ? elevatorFloor : Bfloor
+        Cfloor = Conelevator ? elevatorFloor : Cfloor
+
+
+        if(!Aonelevator &&  Awaiting) {
+                if(Afloor !== elevatorFloor){
+                    return {...state,
+                            elevator: {...state.elevator, destination: Afloor }}
+                }
+                return {...state, 
+                    userA: {...state.userA, onelevator: true, floor: elevatorFloor },
+                    elevator: {...state.elevator, destination: ADestination}}
+        }
+        if(Aonelevator && Awaiting && elevatorFloor === elevatorDestination){
+            return {...state,
+                    userA: {...state.userA, onelevator: false, waiting: false},
+                    elevator: {...state.elevator, destination: null}}
+        }
         
-    return { elevator: {...state.elevator, floor: Nfloor}, userA: {...state.userA, floor: Afloor},
-            userB: {...state.userB, floor: Bfloor},userC: {...state.userC,floor: Cfloor}}
+    
 
     case 'UpdateUserADestination':
         newfloor = parseInt(action.newfloor)
+        return {...state, userA: {...state.userA, destination: newfloor,waiting: true}}
+
+        /*
+        if(AStatus === "OFF"){
+            //new elevator destination equals wherever 
+        }
+        
         AStatus = Afloor === Efloor ? "ON": "OFF"
         const levA = Efloor - newfloor
         const dirA = levA > 0 ? "D" : levA < 0 ? "U" : "N"
@@ -50,8 +86,11 @@ const rootReducer = (state = defaultState, action) => {
                 userA: {...state.userA, destination: newfloor, 
                         status: AStatus,place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirA} }
+                */
     case 'UpdateUserBDestination':
         newfloor = parseInt(action.newfloor)
+        return {...state, userB: {...state.userB, destination: newfloor,waiting: true }}
+        /*
         BStatus = Bfloor === Efloor ? "ON": "OFF"
         const levB = Efloor - newfloor
         const dirB = levB > 0 ? "D" : levB < 0 ? "U" : "N"
@@ -59,8 +98,11 @@ const rootReducer = (state = defaultState, action) => {
                 userB: {...state.userB, destination: newfloor,
                 status: BStatus, place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirB} }
+                */
     case 'UpdateUserCDestination':
         newfloor = parseInt(action.newfloor)
+        return {...state, userC: {...state.userC, destination: newfloor,waiting: true }}
+        /*
         CStatus = Cfloor === Efloor ? "ON": "OFF"
         const levC = Efloor - newfloor
         const dirC = levC > 0 ? "D" : levC < 0 ? "U" : "N"
@@ -68,21 +110,10 @@ const rootReducer = (state = defaultState, action) => {
                 userC: {...state.userC, destination: newfloor,
                 status: CStatus, place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirC} }
+                */
       default:
         return state
     }
 }
-/*
-const userCReducer = (state={Floor: 1,Destination: '',Status: 'OFF'},action) => {
-    switch(action.type){
-      case 'UpdateUserCDestination':
-        return { ...state, Destination: action.newfloor }
-      default:
-        return state
-    }
-  }
-  
-  export default userCReducer
-  */
 
 export default rootReducer
