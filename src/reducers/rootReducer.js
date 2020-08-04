3ad6c705b5e46fec76ee66c538a97341815baf8f
GreatDeveloper66 <ashaffe@gmail.com>
2020-08-03 19:27:38 -0500

    up and down elevator movement bug fixed

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 const defaultState = {
     elevator: {
-        floor: 1,
+        floor: 6,
         direction: 'N'
     },
     userA: {
@@ -26,48 +26,46 @@ const defaultState = {
 }
 
 const rootReducer = (state = defaultState, action) => {
-    let [ Efloor, Afloor, AStatus, ADestination, Bfloor, BStatus, BDestination, Cfloor, CStatus, CDestination, direction, Nfloor ] = 
+    let [ Efloor, Afloor, AStatus, ADestination, Bfloor, BStatus, BDestination, Cfloor, CStatus, CDestination, direction, Nfloor, newfloor ] = 
         [ state.elevator.floor, state.userA.floor, state.userA.status, state.userA.destination,
       state.userB.floor, state.userB.status, state.userB.destination, state.userC.floor,
-      state.userC.status, state.userC.destination, state.elevator.direction, state.elevator.floor ];
+      state.userC.status, state.userC.destination, state.elevator.direction, state.elevator.floor, null ];
 
     switch(action.type) {
     case 'MoveElevatorFloor':
         Nfloor = direction === "U" ? Efloor + 1 : direction === "D" ? Efloor - 1: Efloor
-        if(AStatus === "ON"){
-            Afloor = Nfloor
-        }
-        if(BStatus === "ON"){
-            Bfloor = Nfloor
-        }
-        if(CStatus === "ON"){
-            Cfloor = Nfloor
-        }
-    return { elevator: {...state.elevator, floor: Nfloor}, userA: {...state.userA, floor: Nfloor},
-            userB: {...state.userB, floor: Nfloor},userC: {...state.userC,floor: Nfloor}}
-            
+        Afloor = AStatus === "ON" ? Nfloor : Afloor
+        Bfloor = BStatus === "ON" ? Nfloor: Bfloor
+        Cfloor = CStatus === "ON" ? Nfloor: Cfloor
+        
+    return { elevator: {...state.elevator, floor: Nfloor}, userA: {...state.userA, floor: Afloor},
+            userB: {...state.userB, floor: Bfloor},userC: {...state.userC,floor: Cfloor}}
+
     case 'UpdateUserADestination':
+        newfloor = parseInt(action.newfloor)
         AStatus = Afloor === Efloor ? "ON": "OFF"
-        const levA = Efloor - ADestination
+        const levA = Efloor - newfloor
         const dirA = levA > 0 ? "D" : levA < 0 ? "U" : "N"
         return { ...state, 
-                userA: {...state.userA, destination: action.newfloor, 
+                userA: {...state.userA, destination: newfloor, 
                         status: AStatus,place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirA} }
     case 'UpdateUserBDestination':
+        newfloor = parseInt(action.newfloor)
         BStatus = Bfloor === Efloor ? "ON": "OFF"
-        const levB = Efloor - BDestination
+        const levB = Efloor - newfloor
         const dirB = levB > 0 ? "D" : levB < 0 ? "U" : "N"
         return { ...state, 
-                userB: {...state.userB, destination: action.newfloor,
+                userB: {...state.userB, destination: newfloor,
                 status: BStatus, place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirB} }
     case 'UpdateUserCDestination':
+        newfloor = parseInt(action.newfloor)
         CStatus = Cfloor === Efloor ? "ON": "OFF"
-        const levC = Efloor - CDestination
+        const levC = Efloor - newfloor
         const dirC = levC > 0 ? "D" : levC < 0 ? "U" : "N"
         return { ...state, 
-                userC: {...state.userC, destination: action.newfloor,
+                userC: {...state.userC, destination: newfloor,
                 status: CStatus, place: 'AW'}, 
                 elevator: {...state.elevator, direction: dirC} }
       default:
